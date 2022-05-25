bc37baa6f30eff9da5c7392c7e24af14545fa3d0
Ari Wijaya <ariwijaya.its@gmail.com>
2022-05-25 10:14:58 +0700

    minor change

--- gui.py
+++ gui.py
@@ -28,67 +28,42 @@ class ImageWorker(QThread):
         config_path = os.path.sep.join(['data/', 'obj.cfg'])
         label_path = os.path.sep.join(['data/', 'obj.names'])
         video_path = os.path.sep.join(['data/', 'test.mp4'])
+        image_path = os.path.sep.join(['data/', 'test.jpg'])
 
         confthres=0.5
         nmsthres=0.1
 
         net = get_net(weight_path, config_path)
         classes, colors = get_classes_and_colors(label_path)
-        cap = cv2.VideoCapture(video_path)
-        if not cap.isOpened():
-            print("cannot open media")
-            sys.exit()
 
-        self.startClassification = False
-        while True:
-            _, frame = cap.read()
-            if not _:
-                print("Can't receive frame, Exiting...")
-                break
+        flag = False
 
-            if self.startClassification:
-                height, width = frame.shape[:2]
+        if flag:
+            cap = cv2.VideoCapture(video_path)
+            if not cap.isOpened():
+                print("cannot open media")
+                sys.exit()
 
-                blob = cv2.dnn.blobFromImage(frame, 1/255, (320, 320), (0,0,0), swapRB=True, crop=False)
-                net.setInput(blob)
+            self.startClassification = False
+            while True:
+                _, frame = cap.read()
+                if not _:
+                    print("Can't receive frame, Exiting...")
+                    break
 
-                output_layers_name = net.getUnconnectedOutLayersNames()
-                layer_output = net.forward(output_layers_name)
+                if self.startClassification:
+                    detect(frame, net, classes, colors, confthres, nmsthres)
+                else:
+                    sleep(0.05)
 
-                boxes = []
-                confidances = []
-                class_ids = []
+                ConvertToQtFormat = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+                Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.ImageUpdate.emit(Pic)
 
-                for output in layer_output:
-                    for detection in output:
-                        score = detection[5:]
-                        class_id = np.argmax(score)
-                        confidance = score[class_id]
-                        if confidance > confthres:
-                            box = detection[0:4] * np.array([width, height, width, height])
-                            (centerX, centerY, w, h) = box.astype("int")
-
-                            x = int(centerX - w/2)
-                            y = int(centerY - h/2)
-
-                            boxes.append([x,y,w,h])
-                            confidances.append(float(confidance))
-                            class_ids.append(class_id)
-
-                idxs = cv2.dnn.NMSBoxes(boxes, confidances, confthres, nmsthres)
-
-                if len(idxs) > 0:
-                    for i in idxs.flatten():
-                        x, y, w, h = boxes[i]
-                        color = [int(c) for c in colors[class_ids[i]]]
-                        cv2.rectangle(frame, (x,y), (x+w, y+h), color, 2)
-                        text = "{}: {:.4f}".format(classes[class_ids[i]], confidances[i])
-                        cv2.putText(frame, text, (x, y-5), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 2)
-
-            else:
-                sleep(0.05)
-
-            ConvertToQtFormat = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+        else:
+            image = cv2.imread(image_path)
+            detect(image, net, classes, colors, confthres, nmsthres)
+            ConvertToQtFormat = QImage(image.data, image.shape[1], image.shape[0], QImage.Format_RGB888)
             Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
             self.ImageUpdate.emit(Pic)
 
@@ -114,6 +89,10 @@ class Window(QMainWindow):
 
         self.btnStop = QPushButton("Stop")
         self.btnStop.clicked.connect(self.stop)
+        
+        # temp
+        self.btnStart.setEnabled(False)
+        self.btnStop.setEnabled(False)
 
         layout = QVBoxLayout()
         layout.addWidget(self.image)
